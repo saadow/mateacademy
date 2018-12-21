e23525a5df9a57fb109185d8af46e9c6ed50c194
vadim <vadim.chumasov@gmail.com>
2018-12-21 20:01:54 +0200

    bubble sort completed

--- src/main/java/homework01/BubbleSort.java
+++ src/main/java/homework01/BubbleSort.java
@@ -1,11 +1,24 @@
 package homework01;
 
 public class BubbleSort {
-	
-	
-	
+
+	static void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [] arr = {1,4,3,8,5,5};
+		BubbleSort.bubbleSort(arr);
+		for (int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]);
+		}
 
 	}
 
