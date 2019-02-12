aa0522f75a074463e6929afb04259bb7b6d23863
vadim <vadim.chumasov@gmail.com>
2019-02-13 01:47:25 +0200

    Revert "Revert "hw_jdbc_01""
    This reverts commit 9c19c6b1c378278dfdffddbbcb6ac00455747739.

--- src/main/java/lesson_jdbc_01/OrderDao.java
+++ src/main/java/lesson_jdbc_01/OrderDao.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public interface OrderDao {
 	Set<Order> getAllOrders() throws SQLException;
+	Set<Order> getAllOrdersJoin() throws SQLException;
 	Order findOrderById(BigDecimal id) throws SQLException;
 	boolean insertOrder(Order order) throws SQLException;
 	boolean updateOrder(Order order) throws SQLException;
