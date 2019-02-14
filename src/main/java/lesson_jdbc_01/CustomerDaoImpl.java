085f086865c88df746f0c3a509e410fb1a9bd4d3
vadim <vadim.chumasov@gmail.com>
2019-02-15 00:00:05 +0200

    hw19, jdbc, Insert/Update/Delete

--- src/main/java/lesson_jdbc_01/CustomerDaoImpl.java
+++ src/main/java/lesson_jdbc_01/CustomerDaoImpl.java
@@ -31,7 +31,7 @@ public class CustomerDaoImpl implements CustomerDao {
 	public Set<Customer> findCustomersByCreditLimitMoreThan(BigDecimal creditLimit) throws SQLException {
 		Set<Customer> customers = new HashSet<>();
 		Connection conn = ConnectToDB.getConnection();
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM customers WHERE credit_limit	> ?");
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM customers WHERE credit_limit > ?");
 		stmt.setBigDecimal(1, creditLimit);
 		ResultSet rs = stmt.executeQuery();
 		while (rs.next()) {
@@ -44,6 +44,82 @@ public class CustomerDaoImpl implements CustomerDao {
 		stmt.close();
 		conn.close();
 		return customers;
+	}
+
+	@Override
+	public boolean insertCustomer(Customer customer) throws SQLException {
+		Connection conn = ConnectToDB.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("INSERT INTO customers values (?, ?, ?, ?)");
+		stmt.setBigDecimal(1, customer.getCustNum());
+		stmt.setString(2, customer.getCompany());
+		stmt.setBigDecimal(3, customer.getCustRep());
+		stmt.setBigDecimal(4, customer.getCreditLimit());
+
+		int rowsChanged = stmt.executeUpdate();
+		if (rowsChanged > 0) {
+			stmt.close();
+			conn.close();
+			return true;
+		}
+		stmt.close();
+		conn.close();
+		return false;
+	}
+
+	@Override
+	public boolean updateCustomer(Customer customer) throws SQLException {
+		Connection conn = ConnectToDB.getConnection();
+		String sql = "UPDATE customers SET company=?, credit_limit=?  WHERE cust_num=?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, customer.getCompany());
+		stmt.setBigDecimal(2, customer.getCreditLimit());
+		stmt.setBigDecimal(3, customer.getCustNum());
+		
+		int rowsChanged = stmt.executeUpdate();
+		if (rowsChanged > 0) {
+			stmt.close();
+			conn.close();
+			return true;
+		}
+		stmt.close();
+		conn.close();
+		return false;
+	}
+
+	@Override
+	public boolean deleteCustomer(BigDecimal id) throws SQLException {
+		Connection conn = ConnectToDB.getConnection();
+		String sql = "DELETE FROM customers WHERE cust_num = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setBigDecimal(1, id);
+
+		int rowsChanged = stmt.executeUpdate();
+		if (rowsChanged > 0) {
+			stmt.close();
+			conn.close();
+			return true;
+		}
+		stmt.close();
+		conn.close();
+		return false;
+	}
+
+	@Override
+	public Customer findCustomersByCustNum(BigDecimal cust_num) throws SQLException {
+		Connection conn = ConnectToDB.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM customers WHERE cust_num = ?");
+		stmt.setBigDecimal(1, cust_num);
+		ResultSet rs = stmt.executeQuery();
+
+		Customer customer = null;
+		if (rs.next()) {
+			customer = new Customer(rs.getBigDecimal("cust_num"), rs.getString("company"),
+					rs.getBigDecimal("cust_rep"), rs.getBigDecimal("credit_limit"));
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+		return customer;
 	}
 
 }
--- src/test/java/lesson_jdbc_01/CustomerDaoImplIntegrationTest.java
+++ src/test/java/lesson_jdbc_01/CustomerDaoImplIntegrationTest.java
@@ -1,43 +1,51 @@
 package lesson_jdbc_01;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Set;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerDaoImplIntegrationTest {
 	private static final BigDecimal NOT_EXIST_CUSTOMER = BigDecimal.valueOf(100000000);
 	private static final BigDecimal SOME_RICH_CUSTOMERS = BigDecimal.valueOf(60);
 	private static final Customer MOST_VALUED_CUSTOMER = new Customer(BigDecimal.valueOf(2199), "Супер Пупс",
 			BigDecimal.valueOf(120), BigDecimal.valueOf(150.876));
+	private static final Customer INSERT_DELETE_CUSTOMER = new Customer(BigDecimal.valueOf(11111), "SCOOBY DOOPS",
+			BigDecimal.valueOf(103), BigDecimal.valueOf(120000));
+	private static final Customer UPDATE_CUSTOMER = new Customer(BigDecimal.valueOf(11111), "SCOOBY DOOBY DOOPS",
+			BigDecimal.valueOf(103), BigDecimal.valueOf(12000));
 	private CustomerDao customerDao = new CustomerDaoImpl();
-	
+
 	@Test
-	public void testGetAllCustomers() throws SQLException{
+	public void testGetAllCustomers() throws SQLException {
 		Set<Customer> customers = customerDao.getAllCustomers();
 		System.out.println(customers);
 		assertFalse(customers.isEmpty());
 	}
-	
+
 	@Test
 	public void testFindCustomersByCreditLimitMoreThanEmpty() throws SQLException {
 		Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(NOT_EXIST_CUSTOMER);
 		System.out.println(customers);
 		assertTrue(customers.isEmpty());
 	}
-	
+
 	@Test
 	public void testFindCustomersByCreditLimitMoreThan() throws SQLException {
 		Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(SOME_RICH_CUSTOMERS);
 		System.out.println(customers);
 		assertFalse(customers.isEmpty());
 	}
-	
+
 	@Test
 	public void testFindCustomersByCreditLimitMoreThanHundred() throws SQLException {
 		Set<Customer> customers = customerDao.findCustomersByCreditLimitMoreThan(BigDecimal.valueOf(100));
@@ -45,4 +53,24 @@ public class CustomerDaoImplIntegrationTest {
 		assertTrue(customers.contains(MOST_VALUED_CUSTOMER));
 	}
 
+	@Test
+	public void test1InsertCustomer() throws SQLException {
+		Customer expected = INSERT_DELETE_CUSTOMER;
+		assertTrue(customerDao.insertCustomer(INSERT_DELETE_CUSTOMER));
+		assertEquals(expected, customerDao.findCustomersByCustNum(INSERT_DELETE_CUSTOMER.getCustNum()));
+	}
+
+	@Test
+	public void test2UpdateCustomer() throws SQLException {
+		Customer expected = UPDATE_CUSTOMER;
+		assertTrue(customerDao.updateCustomer(UPDATE_CUSTOMER));
+		assertEquals(expected, customerDao.findCustomersByCustNum(UPDATE_CUSTOMER.getCustNum()));
+		
+	}
+
+	@Test
+	public void test3DeleteCustomer() throws SQLException {
+		assertTrue(customerDao.deleteCustomer(INSERT_DELETE_CUSTOMER.getCustNum()));
+		assertNull(customerDao.findCustomersByCustNum(INSERT_DELETE_CUSTOMER.getCustNum()));
+	}
 }
