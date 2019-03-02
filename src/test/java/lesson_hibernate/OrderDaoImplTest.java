641b6305e4080ea625139fac0e590ba48f341a68
vadim <vadim.chumasov@gmail.com>
2019-03-02 13:21:03 +0200

    hw hibernate

--- src/test/java/lesson_hibernate/OrderDaoImplTest.java
+++ src/test/java/lesson_hibernate/OrderDaoImplTest.java
@@ -1,19 +1,25 @@
 package lesson_hibernate;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
+import java.sql.Date;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import lesson_jpa.Order;
-import lesson_hibernate.OrderDao;
-import lesson_hibernate.OrderDaoImpl;
+import homework_hibernate.OrderDao;
+import homework_hibernate.OrderDaoImpl;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrderDaoImplTest {
 	
+	private static final Order ORDER = new Order(BigDecimal.valueOf(111112), null,
+			Date.valueOf("2019-02-14"), "REI", BigDecimal.valueOf(10), BigDecimal.valueOf(100));
 	private OrderDao orderDao = new OrderDaoImpl();
 
 	@Test
@@ -22,4 +28,26 @@ public class OrderDaoImplTest {
 		System.out.println(order);
 		assertNotNull(order);
 	}
+
+    @Test
+    public void testGetAllOrders(){
+        assertFalse(orderDao.getAllOrders().isEmpty());
+    }
+	
+    @Test
+    public void test1InsertOrder()   {
+        assertTrue(orderDao.insertOrder(ORDER));
+    }
+
+    @Test
+    public void test2UpdateOrder()  {
+        ORDER.setQty(BigDecimal.valueOf(20));
+        assertTrue(orderDao.updateOrder(ORDER));
+    }
+
+    @Test
+    public void test3DeleteOrder() {
+        assertTrue(orderDao.deleteOrder(ORDER.getOrderNum()));
+    }
+
 }
